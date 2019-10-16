aa7ef797dd9e1edf88d4ac9cb662a87c4991f985
浅田和則 <kazu.july05@gmail.com>
2019-10-16 20:21:24 +0900

    Terrain

---
+++ Assets/IgaguriContoroller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgaguriContoroller : MonoBehaviour
+{
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Shoot(new Vector3(0,250,2100));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
